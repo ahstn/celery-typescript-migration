b5ba6900bb83e65af3c5a45e70af87924298f4aa
Adam Houston <adam.houston@bazaarvoice.com>
2022-05-01 10:18:19 +0100

    feat(python): adding sqs support

--- python-simulator/app.py
+++ python-simulator/app.py
@@ -18,7 +18,6 @@ my_app.conf.update(
     ),
 )
 
-
 # NOTE: we have to set the name for tasks manually in order to match the names
 # of the Rust tasks. Otherwise the task names here would be prefixed with 'celery.'.
 @my_app.task(name="add")
@@ -27,9 +26,9 @@ def add(x, y):
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        "celery_app", description="Run a Python Celery producer or consumer"
+        "celery_app", description="Run a Python Celery producer for MQ or SQS"
     )
-    parser.add_argument("mode", choices=["consume", "produce"])
+    parser.add_argument("mode", choices=["mq", "sqs"])
     parser.add_argument(
         "task", nargs="*", choices=["add", "fib", "long_running_task", "bound_task"]
     )
@@ -38,23 +37,24 @@ def parse_args():
 
 def main():
     opts = parse_args()
-    if opts.mode == "consume":
-        sys.argv = [
-            "celery",
-            "--app=celery_app.my_app",
-            "worker",
-            "--queues=celery",
-            "-Ofair",
-            "--loglevel=info",
-        ]
-        _main()
-    else:
-        for task in opts.task:
-            if task == "add":
-                add.apply_async(args=(2, 2))
-            else:
-                add.apply_async(args=(2, 2))
-                my_app.send_task('fib', args=[21])
+    if opts.mode == "sqs":
+        print("sqs")
+        key = os.environ.get("AWS_ACCESS_KEY", "test")
+        secret = os.environ.get("AWS_SECRET_KEY", "test")
+        my_app.conf.broker_url=os.environ.get("SQS_ADDR", f"sqs://{key}:{secret}@127.0.0.1:4566")
+        my_app.conf.update(
+            broker=os.environ.get("SQS_ADDR", f"sqs://{key}:{secret}@127.0.0.1:4566"),
+            broker_transport_options={
+                'region': 'us-east-1',
+            }
+        )
+
+    for task in opts.task:
+        if task == "add":
+            add.apply_async(args=(2, 2))
+        else:
+            add.apply_async(args=(2, 2))
+            my_app.send_task('fib', args=[21])
 
 
 
